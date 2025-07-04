216180b86dc4c1c631cc65848a9ab8f90cf8570d
Suma Hegde <suma.hegde@amd.com>
2025-07-04 07:59:46 +0000

    platform/x86/amd/hsmp: acpi: Add sysfs files to display HSMP telemetry
    Make frequently fetched telemetry available via sysfs. These parameters
    do not fit in hwmon sensor model, hence make them available via sysfs.
    
    Create following sysfs files per acpi device node.
    * c0_residency_input
    * prochot_status
    * smu_fw_version
    * protocol_version
    * ddr_max_bw(GB/s)
    * ddr_utilised_bw_input(GB/s)
    * ddr_utilised_bw_perc_input(%)
    * mclk_input(MHz)
    * fclk_input(MHz)
    * clk_fmax(MHz)
    * clk_fmin(MHz)
    * cclk_freq_limit_input(MHz)
    * pwr_current_active_freq_limit(MHz)
    * pwr_current_active_freq_limit_source
    
    Signed-off-by: Suma Hegde <suma.hegde@amd.com>
    Reviewed-by: Naveen Krishna Chatradhi <naveenkrishna.chatradhi@amd.com>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Link: https://lore.kernel.org/r/20250506101542.200811-3-suma.hegde@amd.com
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- hsmp.c
+++ hsmp.c
@@ -238,6 +238,29 @@ EXPORT_SYMBOL_NS_GPL(hsmp_send_message, "AMD_HSMP");
 EXPORT_SYMBOL_NS_GPL(hsmp_send_message, AMD_HSMP);
 #endif
 
+int hsmp_msg_get_nargs(u16 sock_ind, u32 msg_id, u32 *data, u8 num_args)
+{
+	struct hsmp_message msg = {};
+	unsigned int i;
+	int ret;
+
+	if (!data)
+		return -EINVAL;
+	msg.msg_id = msg_id;
+	msg.sock_ind = sock_ind;
+	msg.response_sz = num_args;
+
+	ret = hsmp_send_message(&msg);
+	if (ret)
+		return ret;
+
+	for (i = 0; i < num_args; i++)
+		data[i] = msg.args[i];
+
+	return 0;
+}
+EXPORT_SYMBOL_NS_GPL(hsmp_msg_get_nargs, "AMD_HSMP");
+
 int hsmp_test(u16 sock_ind, u32 value)
 {
 	struct hsmp_message msg = { 0 };
